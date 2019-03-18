e1fafcc61edd82df637ef6ae65164f7c0fd6358c
Nataniel Martins <nataniel.martins@supero.com.br>
2019-03-18 17:07:47 -0300

    componetization on todolist

--- js/filter/descFilter.js
+++
@@ -1,9 +0,0 @@
-app.filter("UpperDesc", function () {
-	return function (input) {
-		var descr = input.split(" ");
-		var descfinal = descr.map(function (task) {
-			return task.charAt(0).toUpperCase() + task.substring(1).toLowerCase();
-		});
-		return descfinal.join(" ");
-	};
-});
